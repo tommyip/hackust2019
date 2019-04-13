9068217a10110750d7c3d671610b59c2c2492922
mlay1 <michael_lay@live.com>
2019-04-14 01:37:55 +0800

    react router logic

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -5,6 +5,8 @@ import Test from "./test";
 import { compose, withProps, lifecycle } from "recompose";
 import { withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer } from "react-google-maps";
 import googleMapsAPI from "./../ApiKeys"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 const MapWithADirectionsRenderer = compose(
   withProps({
@@ -45,25 +47,68 @@ const MapWithADirectionsRenderer = compose(
   </GoogleMap>
 );
 
+
+function Orders() {
+  return (
+    <div>
+      <div className="col-lg-12">
+          <div className="deliver-card bg-light">
+              <h3 className="w-50 float-left">Order 12345-N</h3>
+              <button className="float-right btn-white deliver-btn">Deliver</button>
+              <div className="clearfix"></div>
+
+              <p>Tseung Kwan O to 12/F Happy Valley</p>
+              <MapWithADirectionsRenderer />
+          </div>
+      </div>
+      <br/>
+      <div className="col-lg-8">
+          <p>16:04 April 13, 2019</p>
+      </div>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+    <h2>About</h2>
+    </div>
+  );
+}
+
+function Topics() {
+  return (
+    <div>
+    <h2>Topics</h2>
+    </div>
+  );
+}
 export default class App extends React.Component {
+
     render () {
         return (
-        <div>
-            <div className="col-lg-12">
-                <div className="deliver-card bg-light">
-                    <h3 className="w-50 float-left">Order 12345-N</h3>
-                    <button className="float-right btn-white deliver-btn">Deliver</button>
-                    <div className="clearfix"></div>
+          <Router>
+            <div>
+              <ul>
+                <li>
+                  <Link to="/">Orders</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/topics">Topics</Link>
+                </li>
+              </ul>
 
-                    <p>Tseung Kwan O to 12/F Happy Valley</p>
-                    <MapWithADirectionsRenderer />
-                </div>
+              <Route exact path="/" component={Orders} />
+              <Route path="/about" component={About} />
+              <Route path="/topics" component={Topics} />
+              <hr />
+
             </div>
-            <br/>
-            <div className="col-lg-8">
-                <p>16:04 April 13, 2019</p>
-            </div>
-        </div>
+          </Router>
         );
     }
 }
