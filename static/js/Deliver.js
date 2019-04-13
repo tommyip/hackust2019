1af2e5cd9158de74fcc587fa1491a5d3d30463ee
Brandon Koh <brandonkhj@Brandons-MacBook-Pro.local>
2019-04-13 23:20:57 +0800

    Ordering

---
+++ static/js/Deliver.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export class Deliver extends React.Component{
+  render(){
+    return <div>
+              <div class="col-lg-8">
+                <h3>Order 12345-N</h3>
+                <button class="pull-right">Deliver</button>
+                <p>Tseung Kwan O to 12/F Happy Valley</p>
+                <map></map>
+                <p>16:04 April 13, 2019</p>
+              </div>
+          </div>;
+  }
+
+
+}
--- static/js/index.jsx
+++ static/js/index.jsx
@@ -2,5 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Deliver from "./Deliver";
 
 ReactDOM.render(<App />, document.getElementById("content"));
