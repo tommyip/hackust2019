034231696c7c9b58d895bc0bd1acfc0af0f1e4e9
Thomas Ip <hkmp7tommy@gmail.com>
2019-04-13 19:47:36 +0800

    Fix url

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/hello")
-def hello():
-    return "Hello World"
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='127.0.0.1', port=4000, debug=True)
